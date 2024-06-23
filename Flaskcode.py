14c09278a47195fa321396af81157c0b6ca1b774
smit02K <kalathiyasmit2001@gmail.com>
2024-06-23 09:04:01 +0530

    new commit

--- Flaskcode.py
+++ Flaskcode.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def mainpage():
-	return "Hello from my python server"
+	return "Hello from your python server"
 
 if(__name__=='__main__'):
 	app.run(host='localhost',debug=True)
